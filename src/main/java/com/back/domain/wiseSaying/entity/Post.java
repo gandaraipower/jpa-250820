5ef07f33825ed52fccece91525095993d5ed4807
gandaraipower <jjangjongsoo@naver.com>
2025-08-20 15:23:46 +0900

    ddl-auto: update , @bean

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -1,21 +1,17 @@
 package com.back.domain.wiseSaying.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
 @Entity
-public class WiseSaying {
+public class Post {
 
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private int id;
-    private String saying;
-    private String author;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
 }
