10a022dc9a567e6219efa4eb6c618430f14a035c
gandaraipower <jjangjongsoo@naver.com>
2025-08-21 11:45:46 +0900

    17강

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -3,7 +3,9 @@ package com.back.domain.post.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-    @Setter
+import java.time.LocalDateTime;
+
+@Setter
     @Getter
     @NoArgsConstructor
     @Entity
@@ -15,8 +17,13 @@ import lombok.*;
         private String title;
         @Column(columnDefinition = "TEXT")
         private String content;
+        private LocalDateTime createdDate;
+        private LocalDateTime modifyDate;
 
         public Post(String title, String content) {
+            this.createdDate = LocalDateTime.now();
+            this.modifyDate = LocalDateTime.now();
+
             this.title=title;
             this.content=content;
         }
